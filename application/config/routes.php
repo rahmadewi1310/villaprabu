4437e9f959188612a69e859738ceacff4ac06e67
I Wayan Mastra <yanmastra59@gmail.com>
2019-08-23 23:52:26 +0800

    first update
    Signed-off-by: I Wayan Mastra <yanmastra59@gmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,11 +49,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'Dashboard';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['pelanggan'] = 'LoginPelanggan';
-$route['housekeeping'] = 'LoginHousekeeping';
-$route['admin'] = 'LoginAdmin';
+// $route['pelanggan'] = 'LoginPelanggan';
+// $route['housekeeping'] = 'LoginHousekeeping';
+// $route['admin'] = 'admin/login';
 
 
