a0903393e70719812be8d64f858c370f86f9202b
iwayanmastra <yanmastra59@gmail.com>
2019-08-28 02:19:29 +0800

    las update, bye bye
    Signed-off-by: iwayanmastra <yanmastra59@gmail.com>

--- application/views/housekeeping/template_housekeeping/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-  <div class="pull-right hidden-xs">
-    <b>Version</b> 2.4.0
-  </div>
-  <strong>Copyright Siti Rahma Dewi</a>.</strong> All rights
-  reserved.
-</footer>
